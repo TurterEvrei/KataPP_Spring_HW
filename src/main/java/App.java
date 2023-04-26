3465e8a8206bbb13daac234e2e86435190887924
belbek <belbek123@gmail.com>
2023-04-26 21:01:29 +0300

    Task completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHW1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanHW2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat1 = applicationContext.getBean("cat", Cat.class);
+        Cat beanCat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Пара HW - " + (beanHW1 == beanHW2));
+        System.out.println("Пара Cat - " + (beanCat1 == beanCat2));
     }
 }
